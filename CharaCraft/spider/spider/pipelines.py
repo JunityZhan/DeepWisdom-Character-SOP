a1107c1e39cc6a62f7089d006f9e3ca7ef7b0a8f
LinkangZhan <linkangzhan@foxmail.com>
2023-10-14 01:48:33 -0400

    add pipelines placeholders

--- CharaCraft/spider/spider/pipelines.py
+++ CharaCraft/spider/spider/pipelines.py
@@ -7,7 +7,20 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+"""
+1. 配置过滤少于多少单词。
+2. 配置过滤关键词。
+3. 配置不过滤带有哪些关键词。
+4. 关键词替换。
+5. 指定网页过滤。
+"""
 
-class SpiderPipeline:
+
+class GeneralPipeline:
+    def process_item(self, item, spider):
+        return item
+
+
+class ConfiguredPipeline:
     def process_item(self, item, spider):
         return item
