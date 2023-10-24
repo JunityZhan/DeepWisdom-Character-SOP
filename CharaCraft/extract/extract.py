f4c449b955bd4ee6f9e0c2eeb808d8a97f036316
LinkangZhan <linkangzhan@foxmail.com>
2023-10-23 22:00:16 -0400

    edit extract

--- CharaCraft/extract/extract.py
+++ CharaCraft/extract/extract.py
@@ -8,15 +8,12 @@ import subprocess
 def parse_arguments():
     """Parse and return command line arguments."""
     parser = argparse.ArgumentParser(description='Run a Scrapy spider.')
-    parser.add_argument('--files', nargs='+', required=True,
-                        help='Target URLs to scrape. At least one URL is required.')
-    parser.add_argument('--name', nargs='+', type=int, required=True,
-                        help='Depth limit for each corresponding URL. Must match the number of URLs provided.')
+    parser.add_argument('--files', nargs='+',
+                        help='Files name of target character in data folder. If not provided, all files will be used.')
+    parser.add_argument('--name', nargs='+', type=str, required=True,
+                        help='Name of target character, you should only provide one character once.')
 
     args = parser.parse_args()
-
-    if len(args.urls) != len(args.depths):
-        raise ValueError('Number of URLs must match the number of depth limits provided.')
 
     return args
 
@@ -25,8 +22,7 @@ def main():
     """Main function to run the Scrapy spider with given arguments."""
     args = parse_arguments()
 
-    for url, depth in zip(args.urls, args.depths):
-        subprocess.run(['python', 'spider_helper.py', url, str(depth), '1' if args.dynamic else '0'])
+
 
 
 if __name__ == '__main__':
